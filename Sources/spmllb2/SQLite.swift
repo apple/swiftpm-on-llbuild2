707096d01e4796137ed2348e95023855b00d1609
Ankit Aggarwal <ankit_aggarwal@apple.com>
2020-07-04 23:24:41 -0700

    Add SQLite support

--- Sources/spmllb2/SQLite.swift
+++ Sources/spmllb2/SQLite.swift
@@ -10,6 +10,8 @@ import ArgumentParser
 import SQLite3
 import TSCBasic
 import TSCUtility
+import TSFCAS
+import TSFCASFileTree
 
 struct SQL: ParsableCommand {
     func run() throws {
@@ -17,9 +19,71 @@ struct SQL: ParsableCommand {
     }
 }
 
-final class SQLite {
+public final class SQLite {
 
-    init() {
+    public init() {
 
+    }
+}
+
+public final class SQLiteCAS: LLBCASDatabase {
+    public let group: LLBFuturesDispatchGroup
+    let sqlite: SQLite
+
+    public init(
+        group: LLBFuturesDispatchGroup,
+        sqlite: SQLite
+    ) {
+        self.group = group
+        self.sqlite = sqlite
+    }
+
+    public func supportedFeatures() -> LLBFuture<LLBCASFeatures> {
+        group.next().makeSucceededFuture(LLBCASFeatures(preservesIDs: true))
+    }
+
+    public func contains(
+        _ id: LLBDataID,
+        _ ctx: Context
+    ) -> LLBFuture<Bool> {
+        fatalError()
+    }
+
+    public func get(
+        _ id: LLBDataID,
+        _ ctx: Context
+    ) -> LLBFuture<LLBCASObject?> {
+        fatalError()
+    }
+
+    public func identify(
+        refs: [LLBDataID],
+        data: LLBByteBuffer,
+        _ ctx: Context
+    ) -> LLBFuture<LLBDataID> {
+        fatalError()
+    }
+
+    public func put(
+        knownID id: LLBDataID,
+        refs: [LLBDataID],
+        data: LLBByteBuffer,
+        _ ctx: Context
+    ) -> LLBFuture<LLBDataID> {
+        fatalError()
+    }
+
+    public func put(
+        refs: [LLBDataID],
+        data: LLBByteBuffer,
+        _ ctx: Context
+    ) -> LLBFuture<LLBDataID> {
+        let knownID = LLBDataID(blake3hash: data, refs: refs)
+        return put(
+            knownID: knownID,
+            refs: refs,
+            data: data,
+            ctx
+        )
     }
 }
